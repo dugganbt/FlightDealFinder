7d4c4f02d5a2d9ae276b75efead92165d2c63c8b
dugganbt <99730314+dugganbt@users.noreply.github.com>
2024-06-06 11:47:33 +0200

    Added search_cheep_flights method.

--- flight_search.py
+++ flight_search.py
@@ -1,15 +1,21 @@
+import json
+
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime, timedelta
 
 # load sheety api variables
 load_dotenv(".env")
 API_KEY = os.getenv("amadeus_api_key")
 API_SECRET = os.getenv("amadeus_api_secret")
 
-ENDPOINT = "https://test.api.amadeus.com/v1/"
-ACCESS_TOKEN_URL = f"{ENDPOINT}security/oauth2/token"
-AIRPORT_CITY_SEARCH_URL = f"{ENDPOINT}reference-data/locations"
+ENDPOINT = "https://test.api.amadeus.com"
+ACCESS_TOKEN_URL = f"{ENDPOINT}/v1/security/oauth2/token"
+AIRPORT_CITY_SEARCH_URL = f"{ENDPOINT}/v1/reference-data/locations"
+
+FLIGHT_OFFERS_URL = f"{ENDPOINT}/v2/shopping/flight-offers"
+
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
@@ -50,5 +56,52 @@ class FlightSearch:
         }
         response = requests.get(AIRPORT_CITY_SEARCH_URL, headers=headers, params=params)
         response.raise_for_status()
-        return response.json()['data'][0]['iataCode']
 
+        try:
+            return response.json()['data'][0]['iataCode']
+        except IndexError:
+            print(f"{city_name} did not return any results from Airport search.")
+
+    def search_cheap_flights(self, origin_city_code="LON", destination_city_code="NYC", time_window_days=180,
+                             currency="EUR"):
+
+        # Defining the time window to search
+        today = datetime.now().today()
+        end_date = today + timedelta(days=time_window_days)
+
+        today = today.strftime("%Y-%m-%d")
+        end_date = end_date.strftime("%Y-%m-%d")
+
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": today,
+            "returnDate": end_date,
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": currency,
+            "max": "10",
+        }
+
+        headers = {
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        response = requests.get(FLIGHT_OFFERS_URL, headers=headers, params=query)
+        response.raise_for_status()
+
+        return response.json()
+
+
+# example
+flight_search = FlightSearch()
+flight_search.search_cheap_flights()
+
+# TODO: Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the
+#  cities in the Google Sheet.
+
+# TODO: If the price is lower than the lowest price listed in the Google
+#  Sheet then send an SMS (or WhatsApp Message) to your own number using the Twilio API.
+
+# TODO: The SMS should include the departure airport IATA code,
+#  destination airport IATA code, flight price and flight dates. e.g.
