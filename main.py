9d0167a1c5e750d5079e817047b56889aeb98bf9
dugganbt <99730314+dugganbt@users.noreply.github.com>
2024-06-08 16:08:27 +0200

    Added notification by whatsapp for flights cheaper than max price desired from google sheet.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
 
+"""
+This is a capstone project from the 100 days of Python code course by Angela Yu. 
+"""
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
 data_manager = DataManager()
 
-# print(data_manager.get_data_rows())
-
-data_manager.add_data_row(city="Boston", lowest_price=2000)
 
 # example
-data_manager = DataManager()
+# add a new destination
+data_manager.add_data_row(city="Boston", lowest_price=2000)
 
+# Perform a flight search
 flight_search = FlightSearch()
 flight_search.search_cheap_flights_for_rows(data_manager.get_data_rows())
